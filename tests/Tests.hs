b79ae843f03aa7a702fb7bfc109d15cd2575ec38
Artem Pelenitsyn <artem.pelenitsyn@gmail.com>
2017-06-17 23:34:46 -0400

    Заработали простые тесты (stack test)

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -21,23 +21,23 @@ singlesTests = testGroup "Tests for single character parsers" $
   concat [testChar, testSat]
 
 -- |Tests for parser char (parses specified character)
--- testChar =
---   [
---     testCase "Consuming one specified char" $
---       parse (char 'c') "c" @?=
---         [('c',((1,1),""))],
---     testCase "Invalid char yields error" $
---       (parse (char 'c') "b") @?=
---         [('',((1,0),"b"))]
---   ]
+testChar =
+  [
+    testCase "Consuming one specified char" $
+      parse (char 'c') "c" @?=
+        [('c',((1,1),""))],
+    testCase "Invalid char yields error" $
+      (parse (char 'c') "b") @?=
+        []
+  ]
 --
 -- -- |Tests for parser sat (parses character only if is satisfies predicate)
--- testSat = [
---   testCase "Consuming one digit" $
---     parse (sat isDigit) "123" @?= [('1',((1,1),"23"))],
---   testCase "Letter is not a digit" $
---     parse (sat isDigit) "a23" @?=[('',((1,0),"a23"))])
---   ]
+testSat = [
+  testCase "Consuming one digit" $
+    parse (sat isDigit) "123" @?= [('1',((1,1),"23"))],
+  testCase "Letter is not a digit" $
+    parse (sat isDigit) "a23" @?= []
+  ]
 
 -------------------------------------------------------------------
 ----------------Test for parsers for groups of chars---------------
@@ -108,7 +108,7 @@ singlesTests = testGroup "Tests for single character parsers" $
 --buildTestList :: (Eq a, Show a) => [(a,a)] -> Test
 --buildTestList = testList . map (uncurry $ testAssertEqual "")
 
-unitTests = testGroup "Unit Tests" [singlesTests,manysTests]
+unitTests = testGroup "Unit Tests" [singlesTests] --,manysTests
 
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
